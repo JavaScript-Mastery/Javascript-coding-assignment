a57cdfb5fe91d38c643638bb782193b9ccc9af73
Ajay-Dhangar <99037494+Ajay-Dhangar@users.noreply.github.com>
2023-04-08 19:44:00 +0530

    Inheritance & Prototype

--- demo.js
+++ demo.js
@@ -73,12 +73,122 @@ let studentRecords = [
 
 // Que.8
 
-let students = [
-  { name: "John", class: "A", rollNo: 1 },
-  { name: "Baba", class: "B", rollNo: 2 },
-  { name: "Yaga", class: "A", rollNo: 3 },
-  { name: "Wick", class: "B", rollNo: 4 },
-  { name: "Alice", class: "A", rollNo: 5 },
-  { name: "Bob", class: "B", rollNo: 6 },
-];
-console.log(students); // [{name: 'John', class: 'A', rollNo: 1 }, {name: 'Baba', class: 'B', rollNo: 2 }, {name: 'Yaga', class: 'A', rollNo: 3 }, {name: 'Wick', class: 'B', rollNo: 4 }, {name: 'Alice', class: 'A', rollNo: 5 }, {name: 'Bob', class: 'B
+// let students = [
+//   { name: "John", class: "A", rollNo: 1 },
+//   { name: "Baba", class: "B", rollNo: 2 },
+//   { name: "Yaga", class: "A", rollNo: 3 },
+//   { name: "Wick", class: "B", rollNo: 4 },
+//   { name: "Alice", class: "A", rollNo: 5 },
+//   { name: "Bob", class: "B", rollNo: 6 },
+// ];
+// console.log(students); // [{name: 'John', class: 'A', rollNo: 1 }, {name: 'Baba', class: 'B', rollNo: 2 }, {name: 'Yaga', class: 'A', rollNo: 3 }, {name: 'Wick', class: 'B', rollNo: 4 }, {name: 'Alice', class: 'A', rollNo: 5 }, {name: 'Bob', class: 'B
+
+// function Device(kind) {
+//   this.kind = kind;
+// }
+// Device.prototype.printkind = function () {
+//   console.log(this.kind);
+// };
+// var product = new Device("music player");
+// product.kind = "radio";
+// product.printkind();
+// Parent object constructor
+// function Parent() {
+//   this.name = "Parent";
+// }
+
+// // Method attached to Parent object
+// Parent.prototype.sayHello = function () {
+//   console.log(`Hello, I am ${this.name}`);
+// };
+
+// // Child object constructor
+// function Child() {
+//   this.name = "Child";
+// }
+
+// // Inheriting from Parent's prototype
+// Child.prototype = Object.create(Parent.prototype);
+// Child.prototype.constructor = Child;
+
+// // Creating a new instance of Child
+// const childObj = new Child();
+
+// // Using Parent's method in Child object
+// childObj.sayHello(); // Output: "Hello, I am Child"
+
+// Parent object constructor
+// function Parent(name) {
+//   this.name = name;
+// }
+
+// // Method attached to Parent object
+// Parent.prototype.sayHello = function () {
+//   console.log(`Hello, I am ${this.name}`);
+// };
+
+// // Child object constructor
+// function Child(name, age) {
+//   Parent.call(this, name); // Call Parent's constructor with Child's instance
+//   this.age = age;
+// }
+
+// // Inheriting from Parent's prototype
+// Child.prototype = Object.create(Parent.prototype);
+// Child.prototype.constructor = Child;
+
+// // Method attached to Child object
+// Child.prototype.sayAge = function () {
+//   console.log(`I am ${this.age} years old`);
+// };
+
+// // Creating instances of Child
+// const child1 = new Child("Child 1", 5);
+// const child2 = new Child("Child 2", 8);
+
+// // Using inherited methods
+// child1.sayHello(); // Output: "Hello, I am Child 1"
+// child1.sayAge(); // Output: "I am 5 years old"
+
+// child2.sayHello(); // Output: "Hello, I am Child 2"
+// child2.sayAge(); // Output: "I am 8 years old"
+function getAllPropertyNames(obj) {
+  let propertyNames = [];
+
+  const getAllProperties = (obj) => {
+    const proto = Object.getPrototypeOf(obj);
+    if (proto !== null) {
+      getAllProperties(proto);
+    }
+
+    const names = Object.getOwnPropertyNames(obj);
+    for (const name of names) {
+      if (!propertyNames.includes(name)) {
+        propertyNames.push(name);
+      }
+    }
+  };
+
+  getAllProperties(obj);
+
+  return propertyNames;
+}
+
+// Example usage:
+
+// Define parent object
+const parent = {
+  parentProp1: "Parent Prop 1",
+  parentProp2: "Parent Prop 2",
+};
+
+// Define child object with inheritance
+const child = Object.create(parent);
+child.childProp1 = "Child Prop 1";
+child.childProp2 = "Child Prop 2";
+
+// Retrieve all property names of child object
+const allPropertyNames = getAllPropertyNames(child);
+
+console.log(allPropertyNames);
+// Output: ["childProp1", "childProp2", "parentProp1", "parentProp2"]
--- extra/index.js
+++ extra/index.js
@@ -1,15 +1,61 @@
-function counter() {
-  var counter = 0;
+document
+  .getElementById("weather-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+    var location = document.getElementById("location-input").value;
+    getWeather(location);
+  });
 
-  function IncreaseCounter() {
-    return (counter += 1);
-  }
+function getWeather(location) {
+  var apiKey = "c811a4e9a01b25fd380ab7c2dea558f8"; // Replace with your OpenWeatherMap API key
+  var apiUrl =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    location +
+    "&appid=" +
+    apiKey;
 
-  return IncreaseCounter;
+  fetch(apiUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      displayWeather(data);
+    })
+    .catch(function (error) {
+      console.log("Error:", error);
+    });
 }
 
-var counter = counter();
-alert(counter());
-alert(counter());
-alert(counter());
-alert(counter());
+function displayWeather(weatherData) {
+  var weatherInfo = document.getElementById("weather-info");
+
+  // Clear previous weather data
+  weatherInfo.innerHTML = "";
+
+  if (weatherData.cod === "404") {
+    weatherInfo.textContent = "Location not found";
+  } else {
+    var cityName = weatherData.name;
+    var temperature = Math.round(weatherData.main.temp - 273.15); // Convert temperature to Celsius
+    var description = weatherData.weather[0].description;
+    var iconUrl =
+      "http://openweathermap.org/img/w/" + weatherData.weather[0].icon + ".png";
+
+    var cityElement = document.createElement("h2");
+    cityElement.textContent = cityName;
+
+    var tempElement = document.createElement("p");
+    tempElement.innerHTML = "Temperature: " + temperature + "&deg;C";
+
+    var descElement = document.createElement("p");
+    descElement.textContent = "Description: " + description;
+
+    var iconElement = document.createElement("img");
+    iconElement.src = iconUrl;
+    iconElement.alt = description;
+    weatherInfo.appendChild(cityElement);
+    weatherInfo.appendChild(tempElement);
+    weatherInfo.appendChild(descElement);
+    weatherInfo.appendChild(iconElement);
+  }
+}
